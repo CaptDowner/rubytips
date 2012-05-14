f921b176a7967acea1a70d774d259f3775ea2f88
CaptDowner <captdowner@comcast.net>
2012-05-14 15:52:39 -0700

    Added method obscure_email to helper to obfuscate email addresses.

--- app/helpers/tips_helper.rb
+++ app/helpers/tips_helper.rb
@@ -1,2 +1,12 @@
 module TipsHelper
+  def obscure_email(email)
+    return nil if email.nil? #Don't bother if the parameter is nil.
+    lower = ('a'..'z').to_a
+    upper = ('A'..'Z').to_a
+    email.split('').map { |char|
+      output = lower.index(char) + 97 if lower.include?(char)
+      output = upper.index(char) + 65 if upper.include?(char)
+      output ? "&##{output};" : (char == '@' ? '&#0064;' : char)
+    }.join
+  end                               
 end
