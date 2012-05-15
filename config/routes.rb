fe18bb3321f122017fa4fde2409cd22584a17392
CaptDowner <captdowner@comcast.net>
2012-05-15 16:04:11 -0700

    Added routes for static pages.

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Tips::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   resources :tips
-
+  resources :pages
   # Sample resource route with options:
   #   resources :products do
   #     member do
@@ -50,7 +50,11 @@ Tips::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'tips#index'
+  # Rails 3
+  match 'about' => 'high_voltage/pages#about', :id => 'about'
+  match 'pages/about' => 'high_voltage/pages#about', :id => 'about'  
+  root :to => 'high_voltage/pages#about', :id => 'about' 
+  #root :to => 'tips#index'
 
   # See how all your routes lay out with "rake routes"
 
