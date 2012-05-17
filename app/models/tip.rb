a2b713b83894c878988fac53258d77924aa37ec1
CaptDowner <captdowner@comcast.net>
2012-05-17 10:29:02 -0700

    Changed tips displayed per page to 20.

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -1,9 +1,9 @@
 class Tip < ActiveRecord::Base
   include PgSearch
   attr_accessible :applies_to, :category, :email, :firstname, :lastname, :posted, :rubytype, :source, :subject, :tip
-  paginates_per 12
+  paginates_per 20
   
-  pg_search_scope :search, :against => [:category, :tip]
+  pg_search_scope :search, :against => [:category, :tip, :email]
 
   def self.text_search(query)
     if query.present?
