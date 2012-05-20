c35a0a1267ca766e20f5562747a0c0cdfc423dea
CaptDowner <captdowner@comcast.net>
2012-05-20 10:30:20 -0700

    Added placeholders for validations.

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -1,8 +1,12 @@
 class Tip < ActiveRecord::Base
   include PgSearch
-  attr_accessible :applies_to, :category, :email, :firstname, :lastname, :posted, :rubytype, :source, :subject, :tip
+  attr_accessible :applies_to, :category, :email, :firstname, :lastname, :posted, :rubytype, :source, :subject, :tip, :xmail
   paginates_per 20
   
+#  validates :xmail, :numericality => true, :allow_nil => true
+#  validates_length_of :xmail, :is => 6, :allow_nil => true, :message => 'Reference must be contain 6 digits'
+#  validates :email, :presence => true, :email => true
+    
   pg_search_scope :search, :against => [:category, :tip, :email]
 
   def self.text_search(query)
