fea789e99659af25695d72f84ef601ca8c213031
CaptDowner <captdowner@comcast.net>
2012-05-31 10:07:43 -0700

    Added PATH and bundler gemspec.

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,8 @@
 require 'rubygems'
 
+Gem.path.push "/home/sdownie/.rvm/gems/ruby-1.9.3-head@rails3"
+Gem::Specification.find_by_name('bundler').activate
+
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
