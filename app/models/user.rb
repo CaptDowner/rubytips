1608d7571fe29f6a534e7b74f88e04af1387888a
CaptDowner <captdowner@gmail.com>
2014-04-10 10:26:51 -0700

    Added self.authenticae method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,9 @@ class User < ActiveRecord::Base
   validates :email, presence: true, 
                     format: VALID_EMAIL_REGEX,
                     uniqueness: { case_sensitive: false }
+
+  def self.authenticate(email, password)
+    user = User.find_by(email: email)
+    user && user.authenticate(password)
+  end
 end
