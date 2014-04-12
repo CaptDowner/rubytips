a57f8b8f26d23212d725ecb312f688720835e209
CaptDowner <captdowner@gmail.com>
2014-04-12 10:46:42 -0700

    Altered db schema.

--- db/schema.rb
+++ db/schema.rb
@@ -9,23 +9,37 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120502223017) do
+ActiveRecord::Schema.define(version: 20140411221217) do
 
-  create_table "tips", :force => true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "tips", force: true do |t|
     t.string   "source"
     t.string   "rubytype"
     t.string   "subject"
     t.string   "category"
-    t.text     "tip"
     t.string   "applies_to"
     t.string   "email"
     t.datetime "posted"
     t.string   "firstname"
     t.string   "lastname"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "xmail"
+    t.text     "tip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "email"
+    t.string   "password_digest"
+    t.boolean  "admin",           default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
