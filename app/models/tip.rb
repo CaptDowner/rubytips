1c638a655f20d89696c26e771dd17a9c80d7bee0
CaptDowner <captdowner@gmail.com>
2014-04-16 08:23:38 -0700

    Removed firstname and lastname from Tip model. Changed email validation regexp.

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -1,31 +1,22 @@
 class Tip < ActiveRecord::Base
+# enables multi field search of PostgreSQL dbs
 include PgSearch
   has_one :user
-#  class EmailValidator < ActiveRecord::Validator
-#    def validate()
-#      record.errors[:email] << "is not valid" unless
-#        record.email =~ /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
-#    end
-#  end
-
-#  validates_with EMailValidator
   paginates_per 20
 
-#  validates :firstname, :presence => true
-#  validates :lastname, :presence => true 
   validates :subject, :presence => true 
   validates :rubytype, :presence => true
   validates :tip, :length => {:minimum => 18}, :presence => true
   validates :xmail, :numericality => true, :allow_nil => true
   validates_length_of :xmail, :is => 6, :allow_nil => true
-  validates :email, :presence => true, allow_nil: true, format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :email, :presence => true, allow_nil: true, format:  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  #    /^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Z‌​a-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$/i 
   validates :posted, :presence => true
   validates :category, :presence => true
   validates :source, :presence => true
   validates :applies_to, :presence => true
 
+  # specify db fields to include for searches
   pg_search_scope :search, :against => [:category, :tip, :email]
 
   def self.text_search(query)
